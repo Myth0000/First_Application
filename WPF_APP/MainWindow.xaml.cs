1ab2d0b3d65990d01a04062ce5919e08fbd34bb0
UserName <Email>
2022-01-01 13:12:11 -0500

    MVVM Toolkit

--- WPF_APP/MainWindow.xaml.cs
+++ WPF_APP/MainWindow.xaml.cs
@@ -1,37 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace WPF_APP
+namespace WPF_APP;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
-        {
-            if(!string.IsNullOrWhiteSpace(txtName.Text) && !FirstNames.Items.Contains(txtName.Text))
-            {
-                FirstNames.Items.Add(txtName.Text);
-                txtName.Clear();
-            }
-        }
+        InitializeComponent();
     }
 }
---
+++ WPF_APP/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace WPF_APP;
+
+public class MainWindowViewModel : ObservableObject
+{
+    private string _newName = "";
+    public RelayCommand AddNameCommand { get; set; }
+    public string NewName { get => _newName; set => SetProperty(ref _newName, value); }
+    public ObservableCollection<string> Names { get; set; } = new();
+
+    public MainWindowViewModel()
+    {
+        AddNameCommand = new(AddName);
+    }
+
+    public void AddName()
+    {
+        if (!string.IsNullOrWhiteSpace(NewName))
+        {
+            Names.Add(NewName);
+            NewName = "";
+        }
+    }
+
+}
